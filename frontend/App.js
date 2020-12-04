c96318962a8362f2371e8cbb304dee412719be0d
Sherryll Elliott <sherryllelliott@gmail.com>
2020-12-04 14:47:35 +0000

    edit video done with 404 error

--- frontend/App.js
+++ frontend/App.js
@@ -5,6 +5,7 @@ import Navbar from './src/components/Navbar'
 import './styles/style.scss'
 import Signup from './src/components/Signup'
 import Login from './src/components/Login'
+import EditVideo from './src/components/EditVideo'
 // ! Some starter code for your frontends, change this
 // ! however you like.
 const App = () => (
@@ -14,6 +15,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/login" component={Login} />
+      <Route exact path ="/edit-video/:videoId" component={EditVideo} />
     </Switch>
   </BrowserRouter>
 )
