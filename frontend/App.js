ce4d226e6945e0abd2a7f2a79c0dbda6a88df4fc
Dec Burns <deaglanburns@msn.com>
2020-12-04 11:42:29 +0000

    navbar

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
 import Home from './src/components/Home'
+import Navbar from './src/components/Navbar'
 import './styles/style.scss'
 
 // ! Some starter code for your frontends, change this
 // ! however you like.
 const App = () => (
   <BrowserRouter>
+    <Navbar />
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
   </BrowserRouter>
 )
-
-
 
 
 export default App
