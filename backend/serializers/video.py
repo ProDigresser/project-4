1419de5f7a5eadbe5356b0fdf9948aae524e762a
Laurence Walshe <laurence_walshe@hotmail.com>
2020-12-07 11:56:18 +0000

    schema stuff

--- backend/serializers/video.py
+++ backend/serializers/video.py
@@ -11,6 +11,7 @@ class VideoSchema(ma.SQLAlchemyAutoSchema, BaseSchema):
     load_instance = True
     load_only = ('user_id',)
   
+  genres = fields.Nested('GenreSchema', many=True, only=('name',))
   user_id = fields.Integer()
   user = fields.Nested('UserSchema', only=('id', 'username'))
     
