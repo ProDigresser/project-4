d7c8061ee38c31505fa746ec924dc9892850b706
Sherryll Elliott <sherryllelliott@gmail.com>
2020-12-08 20:21:58 +0000

    burger formatted and banner added to page

--- frontend/src/components/MobileNavbar.js
+++ frontend/src/components/MobileNavbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import logo from '../styles/logo.jpg'
 import HowTo from '../styles/HowTo.jpg'
-import { slide as Menu } from 'react-burger-menu'
+import { bubble as Menu } from 'react-burger-menu'
 
 const MobileNavbar = (props) => {
 
@@ -19,26 +19,31 @@ const MobileNavbar = (props) => {
   const userId = localStorage.getItem('userId')
   const userName = localStorage.getItem('userName')
 
-  
 
-  return <nav>
-    <Menu>
-      
-      {token && <Link className="menu-item" to="/add-video">Add a Video</Link>}
-         
-      <Link className="menu-item" to="/">Videos</Link>
-     
-      {!token && <Link className="menu-item" to="/login">Log In</Link>}
 
-      {!token && <Link className="menu-item" to="/signup">Sign Up</Link>}
+  return <div className="mobileNavbar">
+    <section className="mobileImage">
+      <img src={HowTo} height={100} alt="logo"/>
+    </section>
+    <nav>
+      <Menu>
 
-      {token && <button className="menu-item"
-        onClick={handleLogout}>
-        Log Out
+        {token && <Link className="menu-item" to="/add-video">Add a Video</Link>}
+
+        <Link className="menu-item" to="/">Videos</Link>
+
+        {!token && <Link className="menu-item" to="/login">Log In</Link>}
+
+        {!token && <Link className="menu-item" to="/signup">Sign Up</Link>}
+
+        {token && <button className="menu-item"
+          onClick={handleLogout}>
+          Log Out
       </button>}
 
-    </Menu>
-  </nav>
+      </Menu>
+    </nav>
+  </div>
 }
 
 export default withRouter(MobileNavbar)
